ad5fa3da99b69c98e47f56b7e3f09b33656911b6
yunfan623@gmail.com <yunfan623@gmail.com>
2021-07-08 10:23:47 +0800

    edit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='icl',
+    version='0.0.0',
+    packages=['icl'],
+    package_dir={'': 'inc'},
+    url='https://github.com/happyxianyu/icl',
+    license='Apache License 2.0',
+    author='happyxianyu',
+    author_email=' happyxianyu623@outlook.com',
+    description='Interval container libarary'
+)
