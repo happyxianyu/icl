7be72ccb4b092c091ae5ca90f676ae0ec59e4b68
happyxianyu <happyxianyu623@outlook.com>
2021-07-24 18:31:01 +0800

    fix

--- test/test_itvset.py
+++ test/test_itvset.py
@@ -23,6 +23,38 @@ def test_iter():
     assert list(s) == make_itvs(l2)
 
 
+def test_or_merge():
+    def generate_sets(n = 10):
+        random.seed(2333)
+        idxs = sorted({random.randint(1, 10000) for _ in range(n)})
+        itvs = Itv(0, 10000).splits(idxs)
+        itvs = [v.create_like() for v in itvs]
+
+        random.shuffle(itvs)
+        itv_sets = [ItvSet([v]) for v in itvs]
+        return itv_sets
+
+    def merge_test_impl(ss, i, j):
+        size = j - i
+        if size < 1:
+            return ItvSet()
+        if size == 1:
+            return ss[i]
+
+        mid = i + size // 2
+        s1 = merge_test_impl(ss, i, mid)
+        s2 = merge_test_impl(ss, mid, j)
+        res = s1 | s2
+        return res
+
+    def merge_test(ss):
+        return merge_test_impl(ss, 0, len(ss))
+
+    itv_sets = generate_sets(1000)
+    s = merge_test(itv_sets)
+    assert s == ItvSet([Itv(-1, 10000)])
+
+
 def test_and():
     # (1, 5) & (3, 7) = (3, 5)
     s1 = ItvSet(make_itvs([(1, 5)]))
@@ -31,39 +63,21 @@ def test_and():
     assert list(s3) == make_itvs([(3, 5)])
 
 
-def test_or():
-    # (1, 5) | (3, 7) = (1, 7)
-    s1 = ItvSet(make_itvs([(1, 5)]))
-    s2 = ItvSet(make_itvs([(3, 7)]))
-    s3 = s1 | s2
-    assert list(s3) == make_itvs([(1, 7)])
+def test_and_1():
+    def generate_sets(n=10, seed=2333):
+        random.seed(seed)
+        idxs = sorted({random.randint(1, 10000) for _ in range(n)})
+        itvs = Itv(0, 10000).splits(idxs, True)
+        itvs = [v for v in itvs]
+        random.shuffle(itvs)
+        itv_sets = [ItvSet([v]) for v in itvs]
+        return itv_sets
 
+    s1 = ItvSet.union(*generate_sets(100, seed = 2333))
+    s2 = ItvSet.union(*generate_sets(100, seed = 2334))
+    s3 = s1&s2
+    print(s1)
+    print(s2)
+    print(s3)
+    print(len(s1),len(s2), len(s3))
 
-def test_or_1():
-    random.seed(2333)
-    idxs = sorted({random.randint(1, 10000) for _ in range(10)})
-    itvs = Itv(0, 10000).splits(idxs)
-    itvs = [v.create_like() for v in itvs]
-
-    print(itvs)
-    random.shuffle(itvs)
-    itv_sets = [ItvSet([v]) for v in itvs]
-
-    def merge1(ss, i, j):
-        size = j - i
-        if size < 1:
-            return ItvSet()
-        if size == 1:
-            return ss[i]
-
-        mid = i + size // 2
-        s1 = merge1(ss, i, mid)
-        s2 = merge1(ss, mid, j)
-        res = s1 | s2
-        return res
-
-    def merge(ss):
-        return merge1(ss, 0, len(ss))
-
-    s = merge(itv_sets)
-    assert s == ItvSet([Itv(0, 10000)])
